458d13f5096d4f46862011b6445e7597a9836bbf
tamarushin <tamarushin@gmail.com>
2018-06-02 11:21:47 -0700

    updated dependencies and added Richards code to mine

--- src/app.js
+++ src/app.js
@@ -5,21 +5,21 @@ const ParsedBitmap = require('./lib/bitmap');
 const fileHandler = require('./lib/file');
 
 fileHandler.readFile(`${__dirname}/../assets/bitmap.bmp`, (error, data) => {
-    if(error){
-        console.log('Error');
-    }
-    else{
-        let bitmap = new ParsedBitmap(data);
-        fileHandler.writeFile(`${__dirname}/../assets/bitmap2.bmp`, bitmap.convertBlackAndWhite(), (err, data) => {
-            if(err) console.log('error writing');
-            else {
-                console.log(data);
-            }
-        })
-        console.log(bitmap);
+  if(error){
+    console.log('Error');
+  }
+  else{
+    let bitmap = new ParsedBitmap(data);
+    fileHandler.writeFile(`${__dirname}/../assets/bitmap2.bmp`, bitmap.convertBlackAndWhite(), (err, data) => {
+      if(err) console.log('error writing');
+      else {
+        console.log(data);
+      }
+    });
+    console.log(bitmap);
 
-    }
-})
+  }
+});
 
 
 
